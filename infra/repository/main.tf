28cb4bd668bd191c57dfd805be432769ae78bcfc
Daniele Manni <dan.manni@gmail.com>
2026-03-20 15:36:01 +0100

    Add jira borards ids

--- infra/repository/main.tf
+++ infra/repository/main.tf
@@ -6,6 +6,7 @@ module "github_repository" {
     name                   = "io-platform-core"
     description            = "The central monorepo for the IO platform ecosystem. Centralizing SDKs and shared packages to ensure developer productivity."
     topics                 = []
+    jira_boards_ids        = ["IOPLT"]
     reviewers_teams        = []
   }
 }
