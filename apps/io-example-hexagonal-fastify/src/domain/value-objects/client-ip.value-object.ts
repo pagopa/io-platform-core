e9a0146161d5906ba300a3aefc5af09b70987dc8
BurnedMarshal <dan.manni@gmail.com>
2026-07-23 12:30:58 +0200

    fix: use ipv4 type for ClientIpSchema value object

--- apps/io-example-hexagonal-fastify/src/domain/value-objects/client-ip.value-object.ts
+++ apps/io-example-hexagonal-fastify/src/domain/value-objects/client-ip.value-object.ts
@@ -1,11 +1,10 @@
-import { NonEmptyStringSchema } from "@pagopa/hexagonal-core/domain/value-objects";
 import { z } from "zod";
 
 /** Unique symbol used to distinguish client addresses from other strings. */
 export const ClientIpBrand = Symbol("ClientIp");
 
 /** Client address resolved from inbound forwarding information. */
-export const ClientIpSchema = NonEmptyStringSchema.brand(ClientIpBrand);
+export const ClientIpSchema = z.ipv4().brand(ClientIpBrand);
 
 /** Validated client address. */
 export type ClientIp = z.infer<typeof ClientIpSchema>;
